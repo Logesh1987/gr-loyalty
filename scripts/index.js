2cc354ef13a7a89f1841127b316da70539efd395
Logesh Kumar <logesh.kumar@krds.fr>
2020-08-24 08:03:54 +0530

    integrate

--- scripts/index.js
+++ scripts/index.js
@@ -3,4 +3,10 @@ var gratisfaction = angular.module('grLoyalty', [])
 gratisfaction.controller('grController', function($scope) {
     $scope.activeMenu = 4;
     $scope.activeClaimCard = null;
+    $scope.loyaltyViews = 1;
+    $scope.hamMenu = false;
+
+    $scope.toggleMenu = e => {
+        $scope.hamMenu = !$scope.hamMenu;
+    }
 })
--- scripts/main.js
+++ scripts/main.js
@@ -4,5 +4,11 @@ var gratisfaction = angular.module('grLoyalty', []);
 gratisfaction.controller('grController', function ($scope) {
   $scope.activeMenu = 4;
   $scope.activeClaimCard = null;
+  $scope.loyaltyViews = 1;
+  $scope.hamMenu = false;
+
+  $scope.toggleMenu = function (e) {
+    $scope.hamMenu = !$scope.hamMenu;
+  };
 });
 //# sourceMappingURL=main.js.map
